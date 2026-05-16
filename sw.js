bead49d7d1582fbcfabde3b447506fffbeda6463
AlQma <abdomakigat@gmail.com>
2026-05-16 14:48:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,20 @@
-// اسم المخزن المؤقت (الكاش)
-const CACHE_NAME = 'hemma-platform-v1';
-
-// قائمة الملفات المطلوب تخزينها ليعمل الموقع بدون إنترنت
-const assetsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;900&display=swap'
+const CACHE_NAME = 'ab-maki-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://i.ibb.co/pBTrM2QM/image.png'
 ];
 
-// تثبيت الـ Service Worker وتخزين الملفات
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('تم فتح الكاش بنجاح');
-      return cache.addAll(assetsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// تفعيل الـ Service Worker وتنظيف الكاش القديم
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('حذف الكاش القديم:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// جلب الملفات من الكاش في حال عدم وجود اتصال بالإنترنت
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
